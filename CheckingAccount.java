cea68088de1a8499c33b80aa960c3c02de4c0b25
JoshYoder1 <josh.yoder@bsu.edu>
2025-03-19 17:47:51 +0000

    update checkingaccount, create Savings account

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -11,21 +11,24 @@ class CheckingAccount implements HasMenu{
 	}//end Main
 
 	public CheckingAccount(){
-		System.out.println("Running Constructer in CheckingAccount.java");
+		//System.out.println("Running Constructer in CheckingAccount.java");
 		balance = 0;
 	}// end CheckingAccount()
 
 	public CheckingAccount(double bal){
-		System.out.println("Running Constructer(1) in CheckingAccount.java");
+		//System.out.println("Running Constructer(1) in CheckingAccount.java");
 		balance = bal;
 	}//end CheckingAccount(1)
 
 	//HasMenu.java Methods
 	public String menu(){
-		System.out.println("Running menu in CheckingAccount.java");
+		//System.out.println("Running menu in CheckingAccount.java");
+		System.out.println("-------------------------");
 		String choice;
 		boolean keepGoing = true;
 
+		System.out.println("Account Menu");
+		System.out.println();
 		System.out.println("0) Quit");
 		System.out.println("1) Check Balance");
 		System.out.println("2) Make a deposit");
@@ -56,7 +59,7 @@ class CheckingAccount implements HasMenu{
 	}//end menu	
 
 	public void start(){
-		System.out.println("Running start in CheckingAccount.java");
+		//System.out.println("Running start in CheckingAccount.java");
 		boolean keepGoing = true;
 
 		while(keepGoing){
@@ -81,36 +84,31 @@ class CheckingAccount implements HasMenu{
 	//End HasMenu.java Methods
 	
 	public double getBalance(){
-		System.out.println("Running getBalance in CheckingAccount.java");
+		//System.out.println("Running getBalance in CheckingAccount.java");
 		double doubbal = Double.valueOf(balance);
 		return(doubbal);
 	}//end getBalance
 
 	public String getBalanceString(){
-		System.out.println("Running getBalanceString in CheckingAccount.java");
+		//System.out.println("Running getBalanceString in CheckingAccount.java");
 		String temp = String.valueOf(balance);
 		return(temp);	
 	}//end getBalanceString
 
 	public void setBalance(){
-		System.out.println("Running setBalance in CheckingAccount.java");
-		System.out.print("Current balance: ");
+		//System.out.println("Running setBalance in CheckingAccount.java");
+		System.out.print("Current balance: $");
 		System.out.format("%.2f", balance);
 		System.out.println();
-		System.out.print("Set new balance: ");
-		try{
-			String temp = input.nextLine();
-			balance = Double.valueOf(temp);
-		} catch(NumberFormatException e){
-			System.out.println("!!!ENTER A NUMBER!!!");
-		}//end try
-		System.out.print("New balance is: ");
+		System.out.print("Set new balance: $");
+		balance = getDouble();
+		System.out.print("New balance is: $");
 		System.out.format("%.2f", balance);
 		System.out.println();
 	}//end setBalance
 
 	public void checkBalance(){
-		System.out.println("Running checkBalance in CheckingAccount.java");
+		//System.out.println("Running checkBalance in CheckingAccount.java");
 		System.out.println("Checking balance...");
 		System.out.print("Current balance: $");
 		System.out.format("%.2f", balance);
@@ -118,15 +116,43 @@ class CheckingAccount implements HasMenu{
 	}//end checkBalance
 
 	private double getDouble(){
-		System.out.println("Running getDouble in CheckingAccount.java");
-		return(0.00);
+		//System.out.println("Running getDouble in CheckingAccount.java");
+		while(true){	
+			try{
+				String temp = input.nextLine();
+				double doub = Double.valueOf(temp);
+				return(doub);
+			} catch(NumberFormatException e){
+				System.out.println("!!!ENTER A NUMBER!!!");
+			}//end exception
+		}//end while
 	}//end getDouble
 
 	public void makeDeposit(){
-		System.out.println("Running makeDeposit in CheckingAccount.java");
+		//System.out.println("Running makeDeposit in CheckingAccount.java");
+		System.out.println("Making a deposit...");
+		System.out.print("How much to deposit? $");
+		double amount = getDouble();
+		balance = balance + amount;
+		System.out.print("New balance: $");
+		System.out.format("%.2f", balance);
+		System.out.println();
 	}//end makeDeposit
 
 	public void makeWithdrawal(){
-		System.out.println("Running makeWithdrawal in CheckingAccount.java");
+		//System.out.println("Running makeWithdrawal in CheckingAccount.java");
+		System.out.println("Making a withdrawal...");
+		System.out.print("How much to withdrawal? $");
+		double amount = getDouble();
+		if(amount > balance){
+			System.out.println("!!!AMOUNT EXCEEDS ACCOUNT BALANCE!!!");
+			System.out.println("ABORTING WITHDRAWAL");
+		}//end if
+		else{
+			balance = balance - amount;
+			System.out.print("New balance: $");
+			System.out.format("%.2f", balance);
+		}//end else
+		System.out.println();
 	}// end makeWithdrawal
 }//end class
---
+++ SavingsAccount.java
@@ -0,0 +1,42 @@
+//SavingsAccount.java
+class SavingsAccount extends CheckingAccount{
+	
+	double interestRate = 0.05;
+	
+	public static void main(String[] args){
+		System.out.println("Running main in SavingsAccount.java");
+		SavingsAccount test = new CheckingAccount(12);
+		test.start();
+		test.setInterestRate();
+		test.getInterestRate();
+		test.calcInterest();
+	}//end main
+
+	public SavingsAccount(){
+		balance = 0;
+	}//end SavingsAccount
+
+	public void calcInterest(){
+		System.out.println("Calculating interest...");
+		System.out.println("Balance: " + balance + "|Interest Rate: " + interestRate * 100 + "%");
+		balance = balance + balance * interestRate;
+		System.out.println("New balance: " + balance);
+	}//end calcInterest
+	
+	public void setInterestRate(){
+		System.out.println("Running setInterestRate in SavingsAccount.java");
+		System.out.print("Set interest rate by entering a percent: ");
+		interestRate = getDouble();
+		interestRate = interestRate * 0.01;
+		System.out.println();
+		System.out.println("Interest rate is:" + interestRate * 100 + "%");
+	}//end setInterestRate
+
+	public double getInterestRate(){
+		System.out.println("Running getInterestRate in SavingsAccount.java");
+		System.out.println("Fetching interest rate...");
+		System.out.println("Interest Rate is:" + interestRate * 100 + "%");
+		System.out.println();
+		return(interestRate);
+	}//end getInterestRate
+}//end class
