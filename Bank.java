a3d45fc6b4e96e5063f394aca9f48e7e13739b08
JoshYoder1 <josh.yoder@bsu.edu>
2025-03-25 14:44:04 +0000

    edit Bank

--- Bank.java
+++ Bank.java
@@ -4,10 +4,14 @@ import java.util.*;
 class Bank implements HasMenu{
 	private Admin admin = new Admin();
 	protected ArrayList<String> customers = new ArrayList<String>();
-
+	private Scabber input = new Scanner(System.in);
 	public Bank(){
 	
 	}//end Bank
+
+	public static void main(String[] args){
+
+	}//end main
 
 	public void loadSampleCustomers(){}//end LoadSampleCustomers
 
@@ -24,7 +28,12 @@ class Bank implements HasMenu{
 	public void loginAsCustomer(){}//end loginAsCustomer
 
 	public String menu(){}//end menu
-
+		System.out.println("-------------------------");
+		System.out.println("Bank Menu"); System.out.println();
+		System.out.println("0) Exit system");
+		System.out.println("1) Login as admin");
+		System.out.println("2) Login as customer");
+		
 	public void start(){}//end start
 
 	public void startAdmin()//end startAdmin
